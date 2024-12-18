68a212a4bdae63b0079d91976abac3986e544e04
deom <my7675306@gmail.com>
2024-12-18 10:53:32 +0900

    update functions

---
+++ analyze.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+import os
+
+def list_all_files(directory):
+    all_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            all_files.append(os.path.join(root, file))
+    return all_files
+
+# 사용 예제
+datas=[]
+directory_path = "data/"
+files = list_all_files(directory_path)
+for file in files:
+    if '.csv' in file:
+        datas.append(file)
+print(datas)
+algolist=['A*','Flood Fill','Rotation Aware(0.5)','Rotation Aware(1)','UF SPS','MHIP','PSO']
+for d in datas:
+    df=pd.read_csv(d)
+    # print(df.head())
+    x=np.arange(len(algolist))
+    moves,turns,time=[],[],[]
+    for algo in algolist:
+        moves.append(df[algo][0])
+        turns.append(df[algo][1])
+        time.append(df[algo][2])
+    shortest_moves_value = min(moves)
+    shortest_moves = [i for i, move in enumerate(moves) if move == shortest_moves_value]
+    shortest_turns_value = min(turns)
+    shortest_turns = [i for i, turn in enumerate(turns) if turn == shortest_turns_value]
+    shortest_time_value = min(time)
+    shortest_time = [i for i, t in enumerate(time) if t == shortest_time_value]
+    
+    print(f'=={d}==')
+    print(f'Shortest in moves: {shortest_moves_value}')
+    print(f'Shortest in turns: {shortest_turns_value}')
+    print(f'Shortest in time: {shortest_time_value}\n')
+    print(f'Best in moves: {", ".join([algolist[i] for i in shortest_moves])}')
+    print(f'Best in turns: {", ".join([algolist[i] for i in shortest_turns])}')
+    print(f'Best in time: {", ".join([algolist[i] for i in shortest_time])}\n')
+    
+    width=0.25
+    plt.bar(x, moves, width, label='moves')
+    plt.bar(x - width, turns, width, label='turns')
+    plt.bar(x + width, time, width, label='time')
+    plt.title(f'{d}')
+    plt.xticks(x, algolist)  # X축 레이블 설정
+    plt.xlabel('cartegory')
+    plt.ylabel('value')
+    plt.legend()
+
+    # 그래프 표시
+    plt.show()
--- main.py
+++ main.py
@@ -125,37 +125,110 @@ def calculate_moves_and_turns_for_path(path):
             turns += 1
     return moves, turns
 
-def update_average(average_move,average_turn,value_move,value_turn):
-    return average_move+value_move,average_turn+value_turn
+def calculate_physical_time_for_path(path):
+    import math
+    # 물리 파라미터
+    a = 1.0      # 가속도 (m/s^2)
+    v_max = 2.0  # 최대 속력 (m/s)
+    d = 1.0      # 각 셀 사이 거리 1m
+    if len(path) < 2:
+        return 0.0
+
+    directions = []
+    for i in range(len(path)-1):
+        r1,c1 = path[i]
+        r2,c2 = path[i+1]
+        dr = r2 - r1
+        dc = c2 - c1
+        directions.append((dr,dc))
+
+    total_time = 0.0
+    v = 0.0
+
+    for i in range(len(directions)):
+        is_turn_ahead = False
+        if i < len(directions)-1:
+            if directions[i] != directions[i+1]:
+                is_turn_ahead = True
+        if is_turn_ahead:
+            disc = v**2 - 2*a
+            if disc < 0:
+                if v==0:
+                    t = math.sqrt(2*d/a)
+                    v_final=0
+                else:
+                    t = math.sqrt(2*d/a)
+                    v_final=0
+            else:
+                t = (v + math.sqrt(v**2 - 2*a))/a
+                v_final = 0
+            total_time += t
+            v = v_final
+        else:
+            d_acc = (v_max**2 - v**2)/(2*a) if v_max**2>v**2 else 0
+            if d_acc <=0:
+                if v>v_max:
+                    v = v_max
+                t = 1.0/v_max
+                v_final = v_max
+            elif d_acc < 1.0:
+                t_acc = (v_max - v)/a
+                dist_cruise = 1.0 - d_acc
+                t_cruise = dist_cruise/v_max
+                t = t_acc + t_cruise
+                v_final = v_max
+            else:
+                A=0.5*a
+                B=v
+                C=-1
+                t = (-B+math.sqrt(B**2 -4*A*C))/(2*A)
+                v_final = v + a*t
+            total_time += t
+            v = v_final
+
+    return total_time
+
+
+def update_average(average_move,average_turn,average_time,value_move,value_turn,value_time):
+    return average_move+value_move,average_turn+value_turn,average_time+value_time
 
 if __name__ == "__main__":
     a_star_average_moves = 0
     a_star_average_turns = 0
+    a_star_average_time = 0
     flood_average_moves = 0
     flood_average_turns = 0
+    flood_average_time = 0
     dfs_average_moves = 0
     dfs_average_turns = 0
+    dfs_average_time = 0
     rotation_average_moves_t1 = 0
     rotation_average_turns_t1 = 0
+    rotation_average_time_t1 = 0
     rotation_average_moves_t05 = 0
     rotation_average_turns_t05 = 0
+    rotation_average_time_t05 = 0
     # rl_average_moves = 0
     # rl_average_turns = 0
     uf_sps_average_moves = 0
     uf_sps_average_turns = 0
+    uf_sps_average_time = 0
     mhip_average_moves = 0
     mhip_average_turns = 0
+    mhip_average_time = 0
     pso_average_moves = 0
     pso_average_turns = 0
-    t=5000
+    pso_average_time = 0
+    n=int(input("maze size(only even number and need to bigger than 20): "))
+    t=int(input("iterations: "))
+    r=int(input("loop factor(0~1000): "))
     import random
     geteven=list(range(20,101,2))
     import tqdm
     for iter in tqdm.tqdm(range(t)):
         # n = random.choice(geteven)
-        n=80
         horizontal_walls, vertical_walls = generate_perfect_maze(n)
-        introduce_loops(n, horizontal_walls, vertical_walls, loop_factor=random.randint(0,250)/1000)
+        introduce_loops(n, horizontal_walls, vertical_walls, loop_factor=random.randint(0,r)/1000)
 
         start = (0,0)
         end = (n-1,n-1)
@@ -180,73 +253,94 @@ if __name__ == "__main__":
         pso_moves, pso_turns = calculate_moves_and_turns_for_path(pso_path if pso_path else [])
         # rl_moves, rl_turns = calculate_moves_and_turns_for_path(rl_path if rl_path else [])
 
+    
+        a_star_time = calculate_physical_time_for_path(a_star_path)
+        flood_time = calculate_physical_time_for_path(flood_path)
+        dfs_time = calculate_physical_time_for_path(dfs_path_)
+        rotation_time_t1 = calculate_physical_time_for_path(rotation_path_t1)
+        rotation_time_t05 = calculate_physical_time_for_path(rotation_path_t05)
+        uf_sps_time = calculate_physical_time_for_path(uf_sps_path)
+        mhip_time = calculate_physical_time_for_path(mhip_path)
+        pso_time = calculate_physical_time_for_path(pso_path)
+        # rl_time = calculate_physical_time_for_path(rl_path)
+
         with open("results.txt", "w", encoding="utf-8") as f:
             f.write("[A*]\n")
-            f.write("moves={}, turns={}\n\n".format(a_star_moves, a_star_turns))
+            f.write("moves={}, turns={}, time={}\n\n".format(a_star_moves, a_star_turns, a_star_time))
 
             f.write("[Flood Fill]\n")
-            f.write("moves={}, turns={}\n\n".format(flood_moves, flood_turns))
+            f.write("moves={}, turns={}, time={}\n\n".format(flood_moves, flood_turns, flood_time))
 
             f.write("[Rotation aware (turn_cost=0.5)]\n")
-            f.write("moves={}, turns={}\n\n".format(rotation_moves_t05, rotation_turns_t05))
+            f.write("moves={}, turns={}, time={}\n\n".format(rotation_moves_t05, rotation_turns_t05, rotation_time_t05))
 
             f.write("[Rotation aware (turn_cost=1)]\n")
-            f.write("moves={}, turns={}\n\n".format(rotation_moves_t1, rotation_turns_t1))
+            f.write("moves={}, turns={}, time={}\n\n".format(rotation_moves_t1, rotation_turns_t1, rotation_time_t1))
 
             f.write("[UF SPS]\n")
-            f.write("moves={}, turns={}\n\n".format(uf_sps_moves, uf_sps_turns))
+            f.write("moves={}, turns={}, time={}\n\n".format(uf_sps_moves, uf_sps_turns, uf_sps_time))
 
             f.write("[MHIP]\n")
-            f.write("moves={}, turns={}\n\n".format(mhip_moves, mhip_turns))
+            f.write("moves={}, turns={}, time={}\n\n".format(mhip_moves, mhip_turns, mhip_time))
 
             f.write("[PSO]\n")
-            f.write("moves={}, turns={}\n\n".format(pso_moves, pso_turns))
+            f.write("moves={}, turns={}, time={}\n\n".format(pso_moves, pso_turns, pso_time))
 
             f.write("[DFS]\n")
-            f.write("moves={}, turns={}\n\n".format(dfs_moves, dfs_turns))
+            f.write("moves={}, turns={}, time={}\n\n".format(dfs_moves, dfs_turns, dfs_time))
 
             # f.write("[RL Learning]\n")
             # f.write("최종경로: 이동={}, 턴={}\n\n".format(rl_moves, rl_turns))
 
-        a_star_average_moves,a_star_average_turns=update_average(a_star_average_moves,a_star_average_turns,a_star_moves,a_star_turns)
-        flood_average_moves,flood_average_turns=update_average(flood_average_moves,flood_average_turns,flood_moves,flood_turns)
-        rotation_average_moves_t05,rotation_average_turns_t05=update_average(rotation_average_moves_t05,rotation_average_turns_t05,rotation_moves_t05,rotation_turns_t05)
-        rotation_average_moves_t1,rotation_average_turns_t1=update_average(rotation_average_moves_t1,rotation_average_turns_t1,rotation_moves_t1,rotation_turns_t1)
-        uf_sps_average_moves,uf_sps_average_turns=update_average(uf_sps_average_moves,uf_sps_average_turns,uf_sps_moves,uf_sps_turns)
-        mhip_average_moves,mhip_average_turns=update_average(mhip_average_moves,mhip_average_turns,mhip_moves,mhip_turns)
-        pso_average_moves,pso_average_turns=update_average(pso_average_moves,pso_average_turns,pso_moves,pso_turns)
-        dfs_average_moves,dfs_average_turns=update_average(dfs_average_moves,dfs_average_turns,dfs_moves,dfs_turns)
+        a_star_average_moves,a_star_average_turns,a_star_average_time=update_average(a_star_average_moves,a_star_average_turns,a_star_average_time,a_star_moves,a_star_turns,a_star_time)
+        flood_average_moves,flood_average_turns,flood_average_time=update_average(flood_average_moves,flood_average_turns,flood_average_time,flood_moves,flood_turns,flood_time)
+        dfs_average_moves,dfs_average_turns,dfs_average_time=update_average(dfs_average_moves,dfs_average_turns,dfs_average_time,dfs_moves,dfs_turns,dfs_time)
+        rotation_average_moves_t1,rotation_average_turns_t1,rotation_average_time_t1=update_average(rotation_average_moves_t1,rotation_average_turns_t1,rotation_average_time_t1,rotation_moves_t1,rotation_turns_t1,rotation_time_t1)
+        rotation_average_moves_t05,rotation_average_turns_t05,rotation_average_time_t05=update_average(rotation_average_moves_t05,rotation_average_turns_t05,rotation_average_time_t05,rotation_moves_t05,rotation_turns_t05,rotation_time_t05)
+        uf_sps_average_moves,uf_sps_average_turns,uf_sps_average_time=update_average(uf_sps_average_moves,uf_sps_average_turns,uf_sps_average_time,uf_sps_moves,uf_sps_turns,uf_sps_time)
+        mhip_average_moves,mhip_average_turns,mhip_average_time=update_average(mhip_average_moves,mhip_average_turns,mhip_average_time,mhip_moves,mhip_turns,mhip_time)
+        pso_average_moves,pso_average_turns,pso_average_time=update_average(pso_average_moves,pso_average_turns,pso_average_time,pso_moves,pso_turns,pso_time)
         
         # if rl_moves != 0:
         #     rl_average_moves += rl_moves
         #     rl_average_turns += rl_turns
-    with open(f'/data/average_res_{n}_{t}.txt','w',encoding='utf-8') as f:
+    import pandas as pd
+    df=pd.DataFrame({"A*":[a_star_average_moves/t,a_star_average_turns/t,a_star_average_time/t],
+                        "Flood Fill":[flood_average_moves/t,flood_average_turns/t,flood_average_time/t],
+                        "Rotation Aware(0.5)":[rotation_average_moves_t05/t,rotation_average_turns_t05/t,rotation_average_time_t05/t],
+                        "Rotation Aware(1)":[rotation_average_moves_t1/t,rotation_average_turns_t1/t,rotation_average_time_t1/t],
+                        "UF SPS":[uf_sps_average_moves/t,uf_sps_average_turns/t,uf_sps_average_time/t],
+                        "MHIP":[mhip_average_moves/t,mhip_average_turns/t,mhip_average_time/t],
+                        "PSO":[pso_average_moves/t,pso_average_turns/t,pso_average_time/t],
+                        "DFS":[dfs_average_moves/t,dfs_average_turns/t,dfs_average_time/t]})
+    df.to_csv(f'data/average_res_{n}_{t}_{r}.csv',index=False)
+    with open(f'data/average_res_{n}_{t}_{r}.txt','w',encoding='utf-8') as f:
         f.write("iterations: {}\n".format(t))
         f.write("maze size: {}\n\n".format(n))
         
         f.write("[A*]\n")
-        f.write("average moves={}, turns={}\n\n".format(a_star_average_moves/t, a_star_average_turns/t))
+        f.write("average moves={}, turns={}, time={}\n\n".format(a_star_average_moves/t, a_star_average_turns/t, a_star_average_time/t))
         
         f.write("[Flood Fill]\n")
-        f.write("average moves={}, turns={}\n\n".format(flood_average_moves/t, flood_average_turns/t))
+        f.write("average moves={}, turns={}, time={}\n\n".format(flood_average_moves/t, flood_average_turns/t, flood_average_time/t))
         
-        f.write("[Rotation aware (turn_cost=0.5)]\n")
-        f.write("average moves={}, turns={}\n\n".format(rotation_average_moves_t05/t, rotation_average_turns_t05/t))
+        f.write("[Rotation aware (0.5)]\n")
+        f.write("average moves={}, turns={}, time={}\n\n".format(rotation_average_moves_t05/t, rotation_average_turns_t05/t, rotation_average_time_t05/t))
         
-        f.write("[Rotation aware (turn_cost=1)]\n")
-        f.write("average moves={}, turns={}\n\n".format(rotation_average_moves_t1/t, rotation_average_turns_t1/t))
+        f.write("[Rotation aware (1)]\n")
+        f.write("average moves={}, turns={}, time={}\n\n".format(rotation_average_moves_t1/t, rotation_average_turns_t1/t, rotation_average_time_t1/t))
         
         f.write("[UF SPS]\n")
-        f.write("average moves={}, turns={}\n\n".format(uf_sps_average_moves/t, uf_sps_average_turns/t))
+        f.write("average moves={}, turns={}, time={}\n\n".format(uf_sps_average_moves/t, uf_sps_average_turns/t, uf_sps_average_time/t))
         
         f.write("[MHIP]\n")
-        f.write("average moves={}, turns={}\n\n".format(mhip_average_moves/t, mhip_average_turns/t))
+        f.write("average moves={}, turns={}, time={}\n\n".format(mhip_average_moves/t, mhip_average_turns/t, mhip_average_time/t))
         
         f.write("[PSO]\n")
-        f.write("average moves={}, turns={}\n\n".format(pso_average_moves/t, pso_average_turns/t))
+        f.write("average moves={}, turns={}, time={}\n\n".format(pso_average_moves/t, pso_average_turns/t, pso_average_time/t))
         
         f.write("[DFS]\n")
-        f.write("average moves={}, turns={}\n\n".format(dfs_average_moves/t, dfs_average_turns/t))
+        f.write("average moves={}, turns={}, time={}\n\n".format(dfs_average_moves/t, dfs_average_turns/t, dfs_average_time/t))
         
         # f.write("[RL Learning]\n")
         # f.write("최종경로: 이동={}, 턴={}\n\n".format(rl_average_moves/t, rl_average_turns/t))
